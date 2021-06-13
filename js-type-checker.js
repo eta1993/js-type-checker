c0fb489da030e747e16b4122e13e94892d7596cc
Elvin Tenorio Aquiatan <64059020+eta1993@users.noreply.github.com>
2021-06-13 13:36:47 +0800

    Uploading initial

---
+++ js-type-checker.js
@@ -0,0 +1,89 @@
+/*jslint
+    node
+    white
+*/
+
+/*property
+    prototype
+    toString
+    call
+    length
+    substring
+    toLowerCase
+    isArray
+    indexOf
+    keys
+    exports
+    inArray
+    isBoolean
+    isDate
+    isEmpty
+    isFunction
+    isNull
+    isNumber
+    isObject
+    isObjectEmpty
+    isString
+    isTruthy
+    isUndefined
+*/
+
+"use strict";
+
+function getType(value) {
+    const begin = 8;
+    const type  = Object.prototype.toString.call(value);
+    const end   = type.length - 1;
+    return type.substring(begin, end).toLowerCase();
+}
+
+const isTruthy = (value)            => Boolean(value);
+const isArray  = (value)            => Array.isArray(value);
+const inArray  = (needle, haystack) => (haystack.indexOf(needle) !== -1);
+
+const isBoolean   = (value) => (getType(value) === "boolean");
+const isDate      = (value) => (getType(value) === "date");
+const isString    = (value) => (getType(value) === "string");
+const isNumber    = (value) => (getType(value) === "number");
+const isFunction  = (value) => (getType(value) === "function");
+const isNull      = (value) => (getType(value) === "null");
+const isUndefined = (value) => (getType(value) === "undefined");
+const isObject    = (value) => (getType(value) === "object");
+
+function getSize(value) {
+    const type = getType(value);
+    if (type === "object") {
+        return (
+            (type === "array")
+            ? value.length
+            : Object.keys(value).length
+        );
+    }
+    return 0;
+}
+
+const isObjectEmpty = (object) => (getSize(object) === 0);
+
+module.exports = {
+    inArray,
+    isArray,
+    isBoolean,
+    isDate,
+    isEmpty(value) {
+        if (!isTruthy(value) || isBoolean(value)) {
+            return true;
+        }
+        if (isObject(value)) {
+            return isObjectEmpty(value);
+        }
+        return false;
+    },
+    isFunction,
+    isNull,
+    isNumber,
+    isObject,
+    isObjectEmpty,
+    isString,
+    isTruthy,
+    isUndefined
+};
